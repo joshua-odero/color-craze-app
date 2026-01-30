d208dee88a264fc6855386843bfd4794192585f1
joshua-odero <misikojodero@gmail.com>
2026-01-30 19:38:02 +0300

    Removed borders from the bookmarks  page

--- src/MainComponents/UserNavBarSection.jsx
+++ src/MainComponents/UserNavBarSection.jsx
@@ -3,15 +3,15 @@ import Hailey from '/kids_avatars/Hailey.png';
 
 function UserNavBarSection() {
   return (
-    <div className="flex items-center justify-center w-full py-5 gap-x-1.5  border">
-      <div className="border w-12 h-12 rounded-full p-1">
+    <div className="flex items-center justify-center w-full py-5 gap-x-1.5">
+      <div className="bg-white w-12 h-12 rounded-full p-1">
         <img src={Hailey} alt="kid_avatar" />
       </div>
-      <div className="flex items-center justify-center bg-white font-bold px-3 py-1 border">
+      <div className="flex items-center justify-center bg-white font-bold px-3 py-1">
         Welcome, [Name]
       </div>
-      <div className="flex items-center justify-center border">
-        <button className="bg-orange-400 rounded-sm w-30 px-3 py-1 font-bold cursor-pointer border">
+      <div className="flex items-center justify-center">
+        <button className="bg-orange-400 rounded-sm w-30 px-3 py-1 font-bold cursor-pointer">
           Logout
         </button>
       </div>
